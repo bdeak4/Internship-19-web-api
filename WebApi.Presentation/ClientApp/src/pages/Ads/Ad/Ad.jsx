d055c1e8a54f4d27feb43b8c80038f9faa4627bd
Bartol Deak <b@bdeak.net>
2022-04-24 21:25:45 +0200

    add ad details page

--- WebApi.Presentation/ClientApp/src/pages/Ads/Ad/Ad.jsx
+++ WebApi.Presentation/ClientApp/src/pages/Ads/Ad/Ad.jsx
@@ -1,11 +1,59 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useGetAdDetail } from "src/api/useAd";
+import Action from "src/components/Action";
+import Loader from "src/components/Loader";
 
 import styles from "./ad.module.scss";
 
 const Ad = () => {
   const { id } = useParams();
-  return <div>view ad {id}</div>;
+  const {
+    data: adDetail,
+    error: adDetailError,
+    isLoading: adDetailIsLoading,
+  } = useGetAdDetail(id);
+
+  if (adDetailIsLoading || !adDetail) {
+    return <Loader />;
+  }
+
+  if (adDetailError) {
+    return <span className="error">Issue getting ad details</span>;
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.heading}>{adDetail.title}</h1>
+      <p>
+        <b>Price:</b> {adDetail.price}
+      </p>
+      <p>
+        <b>Description:</b> {adDetail.description}
+      </p>
+      <p>
+        <b>Category:</b> {adDetail.category?.title}
+      </p>
+      <p>
+        <b>Owner:</b> {adDetail.owner?.email}
+      </p>
+      <p>
+        <b>City:</b> {adDetail.city}
+      </p>
+      <p>
+        <b>County:</b> {adDetail.county}
+      </p>
+      <p>
+        <b>Street:</b> {adDetail.street}
+      </p>
+      <p>
+        <b>View Counter:</b> {adDetail.viewCounter}
+      </p>
+      <Action renderAs="Link" props={{ to: "/ads" }}>
+        &larr; Ads
+      </Action>
+    </div>
+  );
 };
 
 export default Ad;
